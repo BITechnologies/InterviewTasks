32a961d8a2f3888c3ad0dc60095a21efb57f0c67
Wojciech Bardziński <wojciech.bardzinski@bi-technologies.com.pl>
2013-07-02 10:47:36 +0200

    Tests

---
+++ Test/Test/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public IEnumerable<Address> Addresses;
+    }
+
+    public class Address
+    {
+        public int Id;
+        public string City;
+    }
+
+    internal class Class1
+    {
+        public void Reverse<T>(T[] arr)
+        {
+            //Należy zaimplementować w najbardziej optymalny sposób, bez użycia funkcji systemowych.
+            throw new NotImplementedException();
+        }
+
+        public int GetCustomersCountIn(Customer[] customers, string city)
+        {
+            //Należy zaimplementować przy użyciu LINQ
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if an element is present a sorted array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="value"></param>
+        /// <returns>True if the array contains the element, false otherwise</returns>
+        public bool Contains(int[] array, int value)
+        {
+            //Należy zaimplementować przy użyciu algorytmu binary search
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test/Test/Class1Tests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Test
+{
+    [TestFixture]
+    public class Class1Tests
+    {
+        [Test]
+        public void TestReverse()
+        {
+            var c = new Class1();
+            var arr = new[] { 'a', 'b', 'd', 'e' };
+            c.Reverse(arr);
+            CollectionAssert.AreEqual(new[] { 'e', 'd', 'b', 'a' }, arr);
+        }
+
+        [Test]
+        public void TestLinq()
+        {
+            var customers = new [] {
+                new Customer { Id =1, 
+                    Name= "A", 
+                    Addresses = new [] 
+                    {
+                        new Address { Id = 1, City = "Wrocław"},
+                        new Address { Id = 2, City = "Kraków"},
+                    }
+                },
+                new Customer { Id =2, 
+                    Name= "B", 
+                    Addresses = new [] 
+                    {
+                        new Address { Id = 3, City = "Warszawa"},
+                    }
+                },
+                new Customer { Id =3, 
+                    Name= "C", 
+                    Addresses = new [] 
+                    {
+                        new Address { Id = 4, City = "wrocław"},
+                    }
+                }
+            };
+
+            var c = new Class1();
+            var customersInWroclaw = c.GetCustomersCountIn(customers, "Wrocław");
+            Assert.That(customersInWroclaw, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void TestBinarySearch()
+        {
+            var data = Enumerable.Range(1, 100);
+            var divBy3 = data.Where(d => d % 3 == 0).ToArray();
+
+            var c = new Class1();
+            bool val = c.Contains(divBy3, 15);
+            Assert.True(val);
+
+            val = c.Contains(divBy3, 41);
+            Assert.False(val);
+        }
+    }  
+}
